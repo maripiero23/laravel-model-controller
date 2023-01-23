65a34b481c98024819e2a05a44378fecd0a2ed17
maripiero23 <mari.pieropan@gmail.com>
2023-01-23 19:26:32 +0100

    modifiche finali

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    function home(){
+         $movies = Movie::all();
+         return view('home', compact('movies'));
+
+
+    }
+
+
+}
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+@section('content')
+
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 g-4 row-cols-lg-5">
+            @foreach($movies as $movie)
+                <div class="col">
+                    <div class="card border-0">
+                        <img src="https://picsum.photos/200" class="card-img-top img-fluid" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}} title</h5>
+                        <p class="card-text">{{$movie->original_title}}</p>
+                        <p class="card-text">{{$movie->vote}}</p>
+                        <p class="card-text">{{$movie->nationality}}</p>
+                        <p class="card-text">{{$movie->date}}</p>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            
+            @endforeach
+        </div>
+    </div>
+    
+@endsection
